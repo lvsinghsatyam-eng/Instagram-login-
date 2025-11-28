655606ece76a78bd2777281c9b8f1cb0e0b4b3d5
lvsinghsatyam-eng <lvsinghsatyam@gmail.com>
2025-11-29 01:29:40 +0530

    Remove user retrieval and logout functionality

--- joker-script.js
+++ joker-script.js
@@ -1,8 +1,1 @@
-const user = localStorage.getItem('demoUser');
 
-document.getElementById('user').innerText = user ? Hello, ${user} 😈 : '';
-
-function logout() {
-localStorage.clear();
-window.location.href = 'index.html';
-}
